f2b78be2f0575b8052b607cd36cd5adad9ffe9fd
mrfritzg <mrfritz@gamil.com>
2023-04-05 19:57:30 -0400

    fixed navbar flex-direction

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar({ user, setUser }) {
   return (
    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
     <Link to="/repairs" className="navbar-brand mb-0 h1">
-      <img className="d-inline-block align-top" src="/buildinglogo.png" width="50"/>      
+      <img className="d-inline-block align-top" src="/buildinglogo.png" width="75"/>      
     </Link>
     <button type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" className="navbar-toggler" aria-controls="navbarNav" aria-expanded="false" aria-lable="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
