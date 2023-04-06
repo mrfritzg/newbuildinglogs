99d5d24dcd115f6648c8a5412e8f8e135bf17856
mrfritzg <mrfritz@gamil.com>
2023-04-06 06:33:51 -0400

    updated title

--- client/src/pages/users/Login.js
+++ client/src/pages/users/Login.js
@@ -67,7 +67,7 @@ function Login({ setUser }) {
         <div class="container">
           
         <img className="d-inline-block align-top" src="/buildinglogo.png" alt="logo "width="40%"/>
-          <h1>Welcome to MrFritz Luxury Apartments Repair Request Website</h1>
+          <h1>Welcome to MrFritz Luxury Apartments Repair Request</h1>
           <h2>We promise to repair to your issue within <span style={{color:"blue"}}>72 hours</span> or <span style={{color:"red"}}>we pay you 10%</span> of your rent each day until the issue is fixed!</h2>
   <br/>
   <h2>Login</h2>
